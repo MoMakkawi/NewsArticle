bce87ede71cda64343f293483db6d0eb46291ac7
MoMakkawi <MoMakkawi@hotmail.com>
2024-10-26 17:13:12 +0300

    Add DTOs.

---
+++ NewsArticles.Domain/DTOs/NewsArticleDTOs.cs
@@ -0,0 +1,19 @@
+﻿namespace NewsArticles.Domain.DTOs;
+
+internal sealed record NewsArticleDTO(
+    Guid Id,
+    string Title,
+    string Content,
+    List<string> ImagesNames,
+    int ViewsCount,
+    int InteractionsCount,
+    int CommentsCount,
+    DateTime PublishedDate);
+
+internal sealed record NewsArticleWithAuthorDTO(NewsArticleDTO NewsArticleDTO, UserDTO AuthorDTO);
+
+internal sealed record NewsArticleDetailedDTO(
+    NewsArticleDTO NewsArticleDTO,
+    UserDTO AuthorDTO,
+    List<InteractionDTO> InteractionDTOs,
+    List<CommentDTO> CommentDTOs);
---
+++ NewsArticles.Domain/DTOs/UserDTOs.cs
@@ -0,0 +1,11 @@
+﻿namespace NewsArticles.Domain.DTOs;
+
+internal sealed record UserDTO(
+    Guid Id,
+    string FirstName,
+    string LastName,
+    string? ProfileImageName,
+    string Email,
+    string PhoneNumber);
+
+internal sealed record UserWithNewsArticlesDTO(UserDTO UserDTO, List<NewsArticleDTO> NewsArticleDTOs);
