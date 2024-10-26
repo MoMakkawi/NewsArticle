13dbbc6950b037d3b5807fc14a4a7df429613058
MoMakkawi <MoMakkawi@hotmail.com>
2024-10-26 17:12:33 +0300

    Add Consants Roles.

---
+++ NewsArticles.Domain/Constants/Roles.cs
@@ -0,0 +1,8 @@
+﻿namespace NewsArticles.Persistence.Constants;
+
+public static class Roles
+{
+    public const string ADMIN = "ADMIN";
+    public const string COMMENTER = "COMMENTER";
+    public const string AUTHOR = "AUTHOR";
+}
